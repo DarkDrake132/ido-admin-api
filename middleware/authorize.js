99ce95343ad93e6859afb68a8840ca6a3702348b
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2022-06-27 21:18:49 +0700

    edit .env file

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,27 +1,22 @@
-
 const authorizeSuperAdmin = (req, res, next) => {
-    const admin = req.headers.user;
-    if( admin == undefined ){
-        return res.status(403).json({ message: 'User has to be an admin' });
-    }
-    if(admin === 'admin')
-        next();
-    else
-        return res.status(403).json({ message: 'User is not the super admin' })
-}
+  const admin = req.headers.user;
+  if (admin == undefined) {
+    return res.status(403).json({ message: "User has to be an admin" });
+  }
+  if (admin === "admin") next();
+  else return res.status(403).json({ message: "User is not the super admin" });
+};
 
 const authorizeAdmin = (req, res, next) => {
-    const admin = req.headers.user;
-    if( admin == undefined ){
-        return res.status(403).json({ message: 'User has to be an admin' });
-    }
-    if(admin !== 'admin')
-        next();
-    else
-        return res.status(403).json({ message: 'User is not the admin' })
-}
+  const admin = req.headers.user;
+  if (admin == undefined) {
+    return res.status(403).json({ message: "User has to be an admin" });
+  }
+  if (admin !== "admin") next();
+  else return res.status(403).json({ message: "User is not the admin" });
+};
 
 module.exports = {
-    authorizeSuperAdmin,
-    authorizeAdmin,
-}
+  authorizeSuperAdmin,
+  authorizeAdmin,
+};
